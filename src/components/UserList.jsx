4746e623ec2850184b10eadad16949801c56a470
Roxana Scutaru <rm.scutaru@gmail.com>
2020-04-07 14:15:49 +0300

    stilying

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -5,26 +5,26 @@ function UserList(props) {
     const { users } = props;
 
     return (
-        <div >
+        <div className="container">
             <h2>Lista utilizatorilor:</h2>
+                <div className="row">
             { users.map((user, index) => {
                 return (
-                <div className="userList">
-                    <UserItem className="userItem"
-                        id={ user.id }
-                        name={ user.name }
-                        email={ user.email }
-                        isGoldClient={ user.isGoldClient }
-                        salary ={user.salary}
-                        image={ user.image}
-                        key={ index }                    
-                        />
-                    <button className="deleteUser" onClick ={(event)=>props.deleteUserCallBack(event,user.id)}>Sterge utilizatorul:</button>
+                    <div className="col-6">
+                            <UserItem 
+                                id={ user.id }
+                                name={ user.name }
+                                email={ user.email }
+                                isGoldClient={ user.isGoldClient }
+                                salary ={user.salary}
+                                image={ user.image}
+                                key={ index }                    
+                                />
+                            <button className="deleteUser" onClick ={(event)=>props.deleteUserCallBack(event,user.id)}>Sterge utilizatorul:</button>
+                        </div>    
+                    )
+                })} 
                 </div>
-                )
-            })}
-            
-            
         </div>
     );
 }
